8ac870a08768eac55fc4b9d58448704453ed19e2
michkwi039 <michkwi039@student.polsl.pl>
2020-12-17 12:40:56 +0100

    First commit

---
+++ src/main/java/pl/polsl/dinnertime/DinnertimeApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.dinnertime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnertimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnertimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/polsl/dinnertime/DinnertimeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.dinnertime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnertimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
